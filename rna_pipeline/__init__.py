b3340802649cc36063e2eeb3989f55ee4a43c1a4
daisycortesmolbio <daisy@ohalogenetics.com>
2025-10-09 16:28:22 -0400

    init file adjusted

--- rna_pipeline/__init__.py
+++ rna_pipeline/__init__.py
@@ -1,1 +1,8 @@
-__version__ = '0.1.0'
+__version__ = "0.1.0"
+__author__ = "Daisy Cortes"
+__email__ = "daisycortesj@vt.edu"
+
+# Expose a programmatic API only (
+from .main import run_workflow as run  # Import the actual function name
+
+__all__ = ["run", "__version__"]
